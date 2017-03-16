2de630a8d9212cd7146b630d1c66bf9ed17d4e63
Jules LASNE <jlasne@student.42.fr>
2017-03-16 17:46:02 +0100

    Trying stuff :D

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,35 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/03 10:45:49 by jlasne            #+#    #+#             */
-/*   Updated: 2017/03/16 12:51:12 by jlasne           ###   ########.fr       */
+/*   Updated: 2017/03/16 16:53:19 by jlasne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_select.h"
+#include <time.h>
+
+void wait_for (unsigned int secs)
+{
+	unsigned int ret_time;
+	ret_time = time(0) + secs;   // Get finishing time.
+	while (time(0) < ret_time);               // Loop until it arrives.
+}
 
 int			main(int argc, char **argv, char **environ)
 {
 	(void)argc;
 	(void)argv;
 	(void)environ;
-	ft_printf("FT_SELECT");
+	int i;
+
+	i = 1;
+	ft_putstr("\033[?1049h\033[H");
+	while (argv[i] != NULL)
+	{
+		ft_printf("%s ", argv[i]);
+		i++;
+	}
+	wait_for(3);
+	ft_putstr("\033[?1049l");
 	return (0);
 }
